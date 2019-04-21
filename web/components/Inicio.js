a5d6f0cbdf8982a08dd2506d290fa05c95e3c5c1
ZabDS <49504555+ZabDS@users.noreply.github.com>
2019-04-21 00:28:10 -0500

    Add files via upload

---
+++ web/components/Inicio.js
@@ -0,0 +1,12 @@
+class Inicio extends React.Component {
+  render() {
+    return (
+     <div className="App">
+     HOLA
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<Inicio />,
+document.getElementById('App2'));
