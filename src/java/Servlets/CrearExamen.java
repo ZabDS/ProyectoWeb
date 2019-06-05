f1f2fccee88f775c62f0e3c876faaba9053af276
ZabDS <49504555+ZabDS@users.noreply.github.com>
2019-06-05 11:26:42 -0500

    Add files via upload

--- src/java/Servlets/CrearExamen.java
+++ src/java/Servlets/CrearExamen.java
@@ -22,17 +22,52 @@ public class CrearExamen extends HttpServlet {
         throws ServletException, IOException {
             response.setContentType("text/html;charset=UTF-8");
             HttpSession session= request.getSession();
-            String USER,PASS,Preguntas;
+            String USER,PASS,Preguntas,NombreExamen;
             String[] NPreguntas;
             int i=0;
             USER= (String)session.getAttribute("User");
             PASS= (String)session.getAttribute("Pass");
             Preguntas= (String)request.getParameter("Quests");
+            NombreExamen= (String)request.getParameter("TestName");
+            String path = request.getRealPath("/");
             
             NPreguntas=Preguntas.split(",");
             List<String> ANPreguntas = Arrays.asList(NPreguntas);
+            
+            Element raiz = generarRaizXML(NombreExamen, ANPreguntas, USER);
+            
+            crearArchivoXML(raiz, path, NombreExamen);
+            response.sendRedirect("Login");
+            
             PrintWriter out = response.getWriter();
             for(i=0;i<ANPreguntas.size();i++)
             out.println(ANPreguntas.get(i));
+            
+    }
+    
+   
+    protected void crearArchivoXML(Element raiz, String path, String nombre) {
+        Document newdoc = new Document(raiz); //Convierte la raiz a un objeto de tipo document para hacerlo persistente
+        XMLOutputter fmt = new XMLOutputter();
+        File file = new File(path + "/XML/EXAMENES/" + nombre + ".xml");
+        try {
+            FileWriter writer = new FileWriter(file); //Crea el archivo xml
+            fmt.output(newdoc, writer); //escribe en el documento xml
+            writer.flush(); //Vacia todo el buffer en el xml
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    protected Element generarRaizXML(String Nombre, List<String> Preguntas, String Autor) {
+        Element raiz = new Element("Examen"); //permite declarar los element dentro del xml
+        Element autor = new Element("autor");
+        autor.setText(Autor); //Valor del atributo <x>Atributo</x>
+        raiz.addContent(autor);
+        for (int i = 0; i < Preguntas.size(); i++) {
+            raiz.addContent(new Element("Pregunta").setText(Preguntas.get(i)));
+        }
+        return raiz;
     }
 }
